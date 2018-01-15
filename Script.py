2a683112f5fbd602b9cf56be6894fc8e550c4167
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-01-15 00:24:59 -0500

    resolves #9

--- Script.py
+++ Script.py
@@ -1,16 +1,35 @@
-# cd C:\Users\Daniel\Pictures\Twitch\NDSRead
-# python Script.py
-import sys
+#!/usr/bin/env python3
 import shutil
 import time
-import os
 from io import TextIOWrapper
 
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy
+from matplotlib import pyplot
+from matplotlib.pyplot import clf as clear_figures
 
-x = np.arange(1)
+X_ARRAY: numpy.ndarray = numpy.arange(1)
 TEAM_PATH = 'team.txt'
+
+
+# TODO replace this with a StatusCondition enum, if possible
+def condition_from_int(state: int) -> str:
+    status = ''
+    if state == 0:
+        status = 'Healthy'
+    elif state in range(1, 7):  # this is going to give us some trouble
+        status = 'Sleep'
+    elif state == 8:
+        status = 'Poisoned'
+    elif state == 16:
+        status = 'Burned'
+    elif state == 32:
+        status = 'Frozen'
+    elif state == 64:
+        status = 'Paralyzed'
+    elif state == 128:
+        status = 'Toxic'
+
+    return status
 
 
 def fetch_raw_team(handle: TextIOWrapper) -> str:
@@ -19,12 +38,11 @@ def fetch_raw_team(handle: TextIOWrapper) -> str:
 
 def parse_team(raw_team: str) -> list:
     team = []
-    counter = 0
-    Status = ''
+    parse_state = 0
 
     for entry in raw_team.split('\n'):
         line = entry.strip()
-        if counter == 0:
+        if parse_state == 0:
             lst = line.split(',')
             lst2 = lst[0].split(' = ')
             lst3 = lst[2].split(' = ')
@@ -33,57 +51,62 @@ def parse_team(raw_team: str) -> list:
             lst7 = lst[5].split(' = ')
             lst8 = lst[6].split(' = ')
             name = int(lst2[1])
-            HP = int(lst3[1])
-            Max_HP = int(lst4[1])
-            Level = int(lst6[1])
+            health = int(lst3[1])
+            max_hp = int(lst4[1])
+            level = int(lst6[1])
             if int(name) == 0:
-                Status = "Empty"
-                HP = "N"
-                Max_HP = "A"
-                Level = "N/A"
+                status = 'Empty'
+                health = 'N'
+                max_hp = 'A'
+                level = 'N/A'
             elif int(lst8[1]) == 1:
-                name = "egg"
-                Status = "Egg"
-                HP = "N"
-                Max_HP = "A"
-                Level = "N/A"
+                name = 'egg'
+                status = 'Egg'
+                health = 'N'
+                max_hp = 'A'
+                level = 'N/A'
             elif int(lst3[1]) == 0:
-                Status = "Fainted"
-            elif int(lst7[1]) == 0:
-                Status = "Healthy"
-            elif int(lst7[1]) in range(1, 7):
-                Status = "Sleep"
-            elif int(lst7[1]) == 8:
-                Status = "Poisoned"
-            elif int(lst7[1]) == 16:
-                Status = "Burned"
-            elif int(lst7[1]) == 32:
-                Status = "Frozen"
-            elif int(lst7[1]) == 64:
-                Status = "Paralyzed"
-            elif int(lst7[1]) == 128:
-                Status = "Toxic"
-            pokemon = [name, HP, Max_HP, Level, Status]
+                status = 'Fainted'
+            else:
+                status = condition_from_int(int(lst7[1]))
+            pokemon = [name, health, max_hp, level, status]
             team.append(pokemon)
-        if counter == 2:
-            counter = -1
-        counter += 1
+        if parse_state == 2:
+            parse_state = -1
+        parse_state += 1
     return team
 
 
-def make_plot(maxHP,current,output):
-    plt.clf()
-    y = current/maxHP
-    #plt.barh(x, maxHP, color='#607D8B') #green
-    if y > 0.5:
-        plt.barh(x, current, color='#4CAF50', height = 50/96) #green
-    elif y > 0.25 and y <=0.5:
-        plt.barh(x, current, color='#FFC107', height = 50/96) #yellow
+def make_plot(max_hp: int, current_hp: int, out_path: str):
+    clear_figures()
+    health_percent = current_hp / max_hp
+    bar_height = 50 / 96  # TODO make configurable or just less magic
+
+    if health_percent > 0.5:
+        hp_color = '#4CAF50'  # green
+    elif 0.25 < health_percent <= 0.5:
+        hp_color = '#FFC107'  # yellow
     else:
-        plt.barh(x, current, color='#F44336', height = 50/96) #red
-    plt.barh(x, maxHP-current,left = current, color='#212121', height = 50/96) #background
-    plt.axis('off')
-    plt.savefig(output,bbox_inches='tight', pad_inches = 0, transparent=True, dpi = 96)
+        hp_color = '#F44336'  # red
+
+    pyplot.barh(X_ARRAY, current_hp, color=hp_color, height=bar_height)
+
+    # background
+    pyplot.barh(
+            X_ARRAY,
+            max_hp - current_hp,
+            left=current_hp,
+            color='#212121',
+            height=bar_height)
+
+    pyplot.axis('off')
+
+    pyplot.savefig(
+            out_path,
+            bbox_inches='tight',
+            pad_inches=0,
+            transparent=True,
+            dpi=96)
 
 
 def main():
@@ -91,28 +114,37 @@ def main():
     while True:
         team_file = open(TEAM_PATH)
         fresh_state = fetch_raw_team(team_file)
-        if not fresh_state or saved_state == fresh_state:
+        if not fresh_state or fresh_state == saved_state:
             continue
+        team = parse_team(fresh_state)
         saved_state = fresh_state
-        team = parse_team(saved_state)
-        try:
-            for i in range(0, 6):
-                shutil.copyfile("C:\\Users\\Daniel\\Pictures\\Twitch\\sugimori\\" + str(team[i][0]) + ".png",
-                                "C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\team images\\__party" + str(i + 1) + ".png")
-                with open("HP" + str(i + 1) + ".txt", "w") as text_file:
-                    text_file.write("Lvl: " + str(team[i][3]))
-                    text_file.write("\n")
-                    text_file.write("HP: " + str(team[i][1]) + "/" + str(team[i][2]))
-                    text_file.write("\n")
-                    text_file.write("Status: " + str(team[i][4]))
-                if team[i][2] == "A":
-                    shutil.copyfile("C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\Blank.png",
-                                    "C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\health" + str(i + 1) + ".png")
-                else:
-                    make_plot(team[i][2], team[i][1], "health" + str(i + 1) + ".png")
-        except Exception:
-            pass
-        finally:
-            time.sleep(1)
 
-main()
+        try:
+            for i in range(6):  # TODO technically we shouldn't assume 6 pokemon all the time
+                shutil.copyfile(
+                        src=f'C:\\Users\\Daniel\\Pictures\\Twitch\\sugimori\\{team[i][0]}.png',
+                        dst=f'C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\team images\\__party{i + 1}.png')
+
+                status_text = f'Lvl: {team[i][3]}\n' + \
+                              f'HP: {team[i][1]}/{team[i][2]}\n' + \
+                              f'Status: {team[i][4]}'
+                
+                with open(f'HP{i + 1}.txt', mode='w') as text_file:
+                    text_file.write(status_text)
+
+                if team[i][2] == 'A':
+                    shutil.copyfile(
+                            src='C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\Blank.png',
+                            dst=f'C:\\Users\\Daniel\\Pictures\\Twitch\\NDSRead\\health{i + 1}.png')
+                else:
+                    make_plot(
+                            max_hp=team[i][2],
+                            current_hp=team[i][1],
+                            out_path=f'health{i + 1}.png')
+        except Exception:  # TODO narrow down exception or remove the need for try/except here
+            pass
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
